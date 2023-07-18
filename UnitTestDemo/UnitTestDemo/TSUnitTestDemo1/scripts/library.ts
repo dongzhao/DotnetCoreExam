d8fd34c35ca3b92783ee9420134e97ef77bf6319
dong.zhao <dzhaoau@gmail.com>
2023-07-18 17:15:45 +1000

    add typescript unit test

---
+++ UnitTestDemo/UnitTestDemo/TSUnitTestDemo1/scripts/library.ts
@@ -0,0 +1,18 @@
+﻿var jqtest = {
+    showMsg: function (): void {
+        let v: any = jQuery.fn.jquery.toString();
+        let conent: any = $("#ts-example-2")[0].innerHTML;
+        $("#ts-example-2")[0].innerHTML = conent + "" + v + "!!";
+    }
+};
+
+jqtest.showMsg();
+
+function getData(value) {
+    if (value > 1) {
+        return true;
+    }
+};
+
+module.exports = getData;
+
---
+++ UnitTestDemo/UnitTestDemo/TSUnitTestDemo1/tests/library.test.ts
@@ -0,0 +1,10 @@
+﻿import { describe } from "node:test";
+import { it } from "node:test";
+import getData = require("../wwwroot/js/library.js");
+import assert = require("assert");
+
+describe("TestSuit1", function () {
+    it("getData", function () {
+        assert.ok(true, getData(2));
+    })
+})
